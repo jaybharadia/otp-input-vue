0857cbf63324c97397b3c571bdccb2bb5ef102f8
jay-p-b-7span <jay.p.b@7span.com>
2023-06-15 12:33:59 +0530

    Check vue demi done

--- src/install.js
+++ src/install.js
@@ -1,13 +1,19 @@
 import OtpInput from './components/OtpInput.vue';
 import EventBus from './bus';
+import { isVue2, isVue3 } from 'vue-demi'
+
 const VueOtpInput = {
   // eslint-disable-next-line no-unused-vars
   install(Vue, options) {
+  console.log("🚀 ~ file: install.js:8 ~ install ~ Vue:", Vue)
+
+  console.log('isVue3-->', isVue3);
 
   // Let's register our component globally
   // https://vuejs.org/v2/guide/components-registration.html
-    Vue.component('otp-input', OtpInput);
 
+  if( isVue2){
+    Vue.component('otp-input', OtpInput);
     Vue.prototype.$vueOtpInput = {
       // methods:{
         resetOtp : () => {
@@ -15,12 +21,17 @@ const VueOtpInput = {
         }
       // }
     }
+
+    if (typeof window !== 'undefined' && window.Vue) {
+      window.Vue.use(VueOtpInput);
+    }
+
+    
+  }
   },
 };
 
 // Automatic installation if Vue has been added to the global scope.
-if (typeof window !== 'undefined' && window.Vue) {
-  window.Vue.use(VueOtpInput);
-}
+
 
 export default VueOtpInput;
--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue';
 import App from './App.vue';
 Vue.config.productionTip = false;
+import { ref } from 'vue-demi'
+console.log("🚀 ~ file: main.js:5 ~ ref:", ref)
+
+
 
 new Vue({
   render: (h) => h(App),
